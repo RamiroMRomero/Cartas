68f069d7251d19e113e3feaef23505ba1a741eee
RamiroMRomero <ramiromateoromero@sanluis.edu.ar>
2024-03-21 12:04:25 -0300

    comentado ejemplo de constructor

--- src/cartas/Main.java
+++ src/cartas/Main.java
@@ -5,8 +5,8 @@ import static java.lang.System.console;
 
 public class Main {
     public static void main(String[] args) {
-        Carta carta = new Carta(PaloEnum.Basto, 3);
-        System.out.println(carta.toString()); 
+        //Carta carta = new Carta(PaloEnum.Basto, 3);
+        //System.out.println(carta.toString();); 
     }
     
 }
